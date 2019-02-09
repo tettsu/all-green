e36d5f9990ee1f08e66cf16a68be8022f812a679
tettsu <town-field@live.jp>
2019-02-09 12:32:53 +0900

    変数宣言を少し綺麗にした。

--- index.js
+++ index.js
@@ -1,70 +1,30 @@
 // HTMLから取得したグローバル変数
-var akagiPosi1;
-var akagiPosi2;
-var akagiPosi3;
-var akagiPosi4;
-var akagiPosi5;
-var akagiNega1;
-var akagiNega2;
-var akagiNega3;
-var akagiNega4;
-var akagiNega5;
-var boshuPosi1;
-var boshuPosi2;
-var boshuPosi3;
-var boshuPosi4;
-var boshuPosi5;
-var boshuNega1;
-var boshuNega2;
-var boshuNega3;
-var boshuNega4;
-var boshuNega5;
-var chabaPosi1;
-var chabaPosi2;
-var chabaPosi3;
-var chabaPosi4;
-var chabaPosi5;
-var chabaNega1;
-var chabaNega2;
-var chabaNega3;
-var chabaNega4;
-var chabaNega5;
-var ddPosi1;
-var ddPosi2;
-var ddPosi3;
-var ddPosi4;
-var ddPosi5;
-var ddNega1;
-var ddNega2;
-var ddNega3;
-var ddNega4;
-var ddNega5;
+var akagiPosi1,akagiPosi2,akagiPosi3,akagiPosi4,akagiPosi5;
+var akagiNega1,akagiNega2,akagiNega3,akagiNega4,akagiNega5;
+var boshuPosi1,boshuPosi2,boshuPosi3,boshuPosi4,boshuPosi5;
+var boshuNega1,boshuNega2,boshuNega3,boshuNega4,boshuNega5;
+var chabaPosi1,chabaPosi2,chabaPosi3,chabaPosi4,chabaPosi5;
+var chabaNega1,chabaNega2,chabaNega3,chabaNega4,chabaNega5;
+var ddPosi1,ddPosi2,ddPosi3,ddPosi4,ddPosi5;
+var ddNega1,ddNega2,ddNega3,ddNega4,ddNega5;
 
 // +-の集計値
-var akagiPosiSum;
-var akagiNegaSum;
-var boshuPosiSum;
-var boshuNegaSum;
-var chabaPosiSum;
-var chabaNegaSum;
-var ddPosiSum;
-var ddNegaSum;
+var akagiPosiSum,akagiNegaSum;
+var boshuPosiSum,boshuNegaSum;
+var chabaPosiSum,chabaNegaSum;
+var ddPosiSum,ddNegaSum;
 
 // 30,50の集計値
-var akagi30;
-var boshu30;
-var chaba30;
-var dd30;
-var akagi50;
-var boshu50;
-var chaba50;
-var dd50;
+var akagi30,akagi50;
+var boshu30,boshu50;
+var chaba30,chaba50;
+var dd30,dd50;
 
 function calcMain(){
   // HTMLから取得した値でグローバル変数を最新化
-  updateScores();
+  getScores();
 
-  // 各メンバーの+-の計算とHTML書き換え
+  // 各メンバーの+-の計算とtotalの書き換え
   calcAkagiPosi();
   calcAkagiNega();
   calcBoshuPosi();
@@ -74,53 +34,9 @@ function calcMain(){
   calcDdPosi();
   calcDdNega();
 
-  // 30,50の計算と書き換え
+  // 各メンバーの30,50の計算と書き換え
   calcRate30();
   calcRate50();
-}
-
-function calcRate30(){
-  var akagiResult = parseFloat(akagiPosiSum) - parseFloat(akagiNegaSum);
-  akagiResult *= 30;
-  var target = document.getElementById("30Akagi");
-  target.innerText = akagiResult;
-
-  var boshuResult = parseFloat(boshuPosiSum) - parseFloat(boshuNegaSum);
-  boshuResult *= 30;
-  var target = document.getElementById("30Boshu");
-  target.innerText = boshuResult;
-
-  var chabaResult = parseFloat(chabaPosiSum) - parseFloat(chabaNegaSum);
-  chabaResult *= 30;
-  var target = document.getElementById("30Chaba");
-  target.innerText = chabaResult;
-
-  var ddResult = parseFloat(ddPosiSum) - parseFloat(ddNegaSum);
-  ddResult *= 30;
-  var target = document.getElementById("30Dd");
-  target.innerText = ddResult;
-}
-
-function calcRate50(){
-  var akagiResult = parseFloat(akagiPosiSum) - parseFloat(akagiNegaSum);
-  akagiResult *= 50;
-  var target = document.getElementById("50Akagi");
-  target.innerText = akagiResult;
-
-  var boshuResult = parseFloat(boshuPosiSum) - parseFloat(boshuNegaSum);
-  boshuResult *= 50;
-  var target = document.getElementById("50Boshu");
-  target.innerText = boshuResult;
-
-  var chabaResult = parseFloat(chabaPosiSum) - parseFloat(chabaNegaSum);
-  chabaResult *= 50;
-  var target = document.getElementById("50Chaba");
-  target.innerText = chabaResult;
-
-  var ddResult = parseFloat(ddPosiSum) - parseFloat(ddNegaSum);
-  ddResult *= 50;
-  var target = document.getElementById("50Dd");
-  target.innerText = ddResult;
 }
 
 function calcAkagiPosi(){
@@ -171,7 +87,43 @@ function calcDdNega(){
   target.innerText = ddNegaSum;
 }
 
-function updateScores(){
+function calcRate30(){
+  akagi30 = (parseFloat(akagiPosiSum) - parseFloat(akagiNegaSum)) * 30;
+  var target = document.getElementById("30Akagi");
+  target.innerText = akagi30;
+
+  boshu30 = (parseFloat(boshuPosiSum) - parseFloat(boshuNegaSum)) * 30;
+  var target = document.getElementById("30Boshu");
+  target.innerText = boshu30;
+
+  chaba30 = (parseFloat(chabaPosiSum) - parseFloat(chabaNegaSum)) * 30;
+  var target = document.getElementById("30Chaba");
+  target.innerText = chaba30;
+
+  dd30 = (parseFloat(ddPosiSum) - parseFloat(ddNegaSum)) * 30;
+  var target = document.getElementById("30Dd");
+  target.innerText = dd30;
+}
+
+function calcRate50(){
+  akagi50 = (parseFloat(akagiPosiSum) - parseFloat(akagiNegaSum)) * 50;
+  var target = document.getElementById("50Akagi");
+  target.innerText = akagi50;
+
+  boshu50 = (parseFloat(boshuPosiSum) - parseFloat(boshuNegaSum)) * 50;
+  var target = document.getElementById("50Boshu");
+  target.innerText = boshu50;
+
+  chaba50 = (parseFloat(chabaPosiSum) - parseFloat(chabaNegaSum)) * 50;
+  var target = document.getElementById("50Chaba");
+  target.innerText = chaba50;
+
+  dd50 = (parseFloat(ddPosiSum) - parseFloat(ddNegaSum)) * 50;
+  var target = document.getElementById("50Dd");
+  target.innerText = dd50;
+}
+
+function getScores(){
   akagiPosi1 = parseFloat(document.getElementById("1Akagi+").value|0);
   akagiPosi2 = parseFloat(document.getElementById("2Akagi+").value|0);
   akagiPosi3 = parseFloat(document.getElementById("3Akagi+").value|0);
